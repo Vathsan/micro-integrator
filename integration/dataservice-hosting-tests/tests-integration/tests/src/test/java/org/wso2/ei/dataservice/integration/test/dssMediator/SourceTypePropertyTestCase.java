e730ed51001c6adaeedca27d59b511ffcfa67016
Vathsan <srivathsanmail@gmail.com>
2020-09-17 17:18:12 +0530

    Add DSSCallMediator test case

---
+++ integration/dataservice-hosting-tests/tests-integration/tests/src/test/java/org/wso2/ei/dataservice/integration/test/dssMediator/SourceTypePropertyTestCase.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.ei.dataservice.integration.test.dssMediator;
+
+import org.apache.http.HttpResponse;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.wso2.esb.integration.common.utils.ESBIntegrationTest;
+import org.wso2.esb.integration.common.utils.clients.SimpleHttpClient;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.testng.Assert.assertTrue;
+
+public class SourceTypePropertyTestCase extends ESBIntegrationTest {
+
+    @BeforeClass(alwaysRun = true)
+    public void serviceDeployment() throws Exception {
+        super.init();
+        verifyProxyServiceExistence("sourceTypePropertyProxy");
+    }
+
+    @Test(groups = {"wso2.esb"}, description = "Test DSS Call mediator when " +
+            "source type is configured to property ")
+    public void testDSSCallMediatorSourceProperty() throws IOException {
+        SimpleHttpClient simpleHttpClient = new SimpleHttpClient();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Accept", "application/xml");
+        HttpResponse response = simpleHttpClient.doGet(getProxyServiceURLHttp("sourceTypePropertyProxy"), headers);
+        String responsePayload = simpleHttpClient.getResponsePayload(response);
+
+        assertTrue(responsePayload.contains("SUCCESSFUL"), "Error adding records with source type property configuration");
+    }
+
+    @AfterClass
+    public void cleanUp() throws Exception {
+        super.cleanup();
+    }
+
+}
